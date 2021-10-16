a2796858f7431fd11409811663cca939d5379c1d
purankuton2001 <63197380+purankuton2001@users.noreply.github.com>
2021-10-16 20:41:19 +0900

    Update App.js

--- App.js
+++ App.js
@@ -43,13 +43,13 @@ export const EditorContext = createContext(InitialEditorState);
 
 Orientation.lockToPortrait();
 const firebaseConfig = {
-  apiKey: 'AIzaSyA5OGbz6Ti2e9SgbDgIKvaxpL_MSj3FZxA',
-  authDomain: 'hitokoto-309511.firebaseapp.com',
-  projectId: 'hitokoto-309511',
-  storageBucket: 'hitokoto-309511.appspot.com',
-  messagingSenderId: '56470386968',
-  appId: '1:56470386968:web:a00d99c32e013139bc866d',
-  measurementId: 'G-5VDCBVVEF3',
+  apiKey: ,
+  authDomain: ,
+  projectId: ,
+  storageBucket:,
+  messagingSenderId:,
+  appId: ,
+  measurementId:,
 };
 
 if (firebase.apps.length === 0) {
