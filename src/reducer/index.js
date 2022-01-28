1a4a0562a9d34774d66be62afbabaf133ffe67b6
purankuton2001 <purankuton2001@icloud.com>
2022-01-28 11:48:01 +0900

    debug reanimated

---
+++ src/reducer/index.js
@@ -0,0 +1,111 @@
+import TrackPlayer from 'react-native-track-player';
+
+const itemsUpdate = (items, index) => {
+  TrackPlayer.reset().then(() => {
+    const recordOptions = {
+      ratingType: TrackPlayer.RATING_THUMBS_UP_DOWN,
+      stopWithApp: true,
+      previousIcon: require('./src/Icon/dislike_on.png'),
+      nextIcon: require('./src/Icon/dislike_on.png'),
+      capabilities: [
+        TrackPlayer.CAPABILITY_PLAY,
+        TrackPlayer.CAPABILITY_PAUSE,
+        TrackPlayer.CAPABILITY_SKIP_TO_NEXT,
+        TrackPlayer.CAPABILITY_SKIP_TO_PREVIOUS,
+      ],
+      notificationCapabilities: [
+        TrackPlayer.CAPABILITY_PLAY,
+        TrackPlayer.CAPABILITY_PAUSE,
+        TrackPlayer.CAPABILITY_SKIP_TO_NEXT,
+        TrackPlayer.CAPABILITY_SKIP_TO_PREVIOUS,
+      ],
+      compactCapabilities: [
+        TrackPlayer.CAPABILITY_PLAY,
+        TrackPlayer.CAPABILITY_PAUSE,
+      ],
+    };
+    TrackPlayer.updateOptions(recordOptions);
+    const item = [];
+    items.forEach(i => {
+      item.push({
+        playLists: i.playLists,
+        duration: i.duration,
+        id: i.id,
+        title: i.title,
+        url: i.url,
+        genre: i.genre,
+        artwork: i.artwork,
+        date: i.date,
+        artist: i.artist.name,
+      });
+    });
+    TrackPlayer.add(item).then(() => {
+      TrackPlayer.play();
+    });
+  });
+  return {index, items, item: items[index], playerIsVisible: true};
+};
+
+export const playerReducer = (oldState, action) => {
+  console.log(oldState);
+  console.log(action);
+  switch (action.type) {
+    case 'ITEMSUPDATE':
+      return {items: action.data};
+    case 'CONTENTSSELECT':
+      const k = itemsUpdate(action.items, action.index);
+      return k;
+    case 'TRACKCHANGE':
+      return {...oldState, index: oldState.index + 1};
+    case 'PLAYERTOGGLEOPEN':
+      return {...oldState, playerIsVisible: !oldState.playerIsVisible};
+    case 'SETLIKE':
+      return {...oldState, likes: action.likes};
+  }
+};
+export const editorReducer = (oldState, action) => {
+  console.log(oldState);
+  console.log(action);
+  switch (action.type) {
+    case 'RANGECHANGE':
+      return {...oldState, displayRange: action.displayRange};
+    case 'TOGGLEPLAY':
+      return {...oldState, playing: !oldState.playing};
+    case 'PAUSE':
+      return {...oldState, playing: false};
+    case 'RECORDSADD':
+      return {
+        ...oldState,
+        duration:
+          action.records.end > oldState.duration
+            ? action.records.end
+            : oldState.duration,
+        records: [...oldState.records, action.records],
+      };
+    case 'URLPOST':
+      oldState.records[action.id].url = action.url;
+      return {
+        oldState,
+      };
+    case 'SEEK':
+      return {...oldState, currentTime: action.value};
+    case 'SEEKSTART':
+      return {...oldState, start: action.value};
+    case 'SEEKEND':
+      return {...oldState, end: action.value};
+    case 'TIMEPASS':
+      return {...oldState, currentTime: oldState.currentTime + 0.1};
+    case 'SEEKRECORDS':
+      oldState.records[action.id].start = action.value;
+      oldState.records[action.id].end =
+        action.value +
+        oldState.records[action.id].trimEnd -
+        oldState.records[action.id].trimStart;
+      return oldState;
+    case 'RECORDSEDIT':
+      return {
+        ...oldState,
+        records: oldState.records.splice(action.index, 1, action.records),
+      };
+  }
+};
--- src/utils/index.js
+++ src/utils/index.js
@@ -54,3 +54,13 @@ export function translateError(code) {
   }
   return message;
 }
+
+export const firebaseConfig = {
+  apiKey: 'AIzaSyA5OGbz6Ti2e9SgbDgIKvaxpL_MSj3FZxA',
+  authDomain: 'hitokoto-309511.firebaseapp.com',
+  projectId: 'hitokoto-309511',
+  storageBucket: 'hitokoto-309511.appspot.com',
+  messagingSenderId: '56470386968',
+  appId: '1:56470386968:web:a00d99c32e013139bc866d',
+  measurementId: 'G-5VDCBVVEF3',
+};
